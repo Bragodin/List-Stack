147069dc855dc50b0e5c7ba52f2a56a9e5a50ded
Drozd Egor <drozdegor.sr@gmail.com>
2019-03-01 18:09:07 +0300

    firs commit

--- list.ts
+++ list.ts
@@ -1,1 +1,15 @@
- 
+class LinkedList<T> {
+    head: T;
+    length: number;
+    constructor(value: T){
+        this.length = 0;
+        this.head = null;
+    }
+    add(value: T){
+        const node: any = { value };
+        node.next = this.head;
+        this.head = node;
+        this.length++;
+        return this;
+    }
+}
