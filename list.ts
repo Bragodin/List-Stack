5eee2a7fcf05d8ce541b9782b7db83d1163454d1
Drozd Egor <drozdegor.sr@gmail.com>
2019-03-02 18:48:44 +0300

    add

--- list.ts
+++ list.ts
@@ -5,11 +5,18 @@ class LinkedList<T> {
         this.length = 0;
         this.head = null;
     }
+    // object
     add(value: T){
         const node: any = { value };
         node.next = this.head;
         this.head = node;
         this.length++;
         return this;
+    }
+    remove(){
+        const value: any = this.head.value;
+        this.head = this.head.next;
+        this.length--;
+        return value;
     }
 }
