c57b0201a33b6426cf0863d6460af2cfbedd86ef
Drozd Egor <drozdegor.sr@gmail.com>
2019-03-04 01:23:40 +0300

    hashcode&equals

--- stack.ts
+++ stack.ts
@@ -9,7 +9,7 @@ class Stack<T>{
         const len = ++this.length;
         this.storage[len] = value;
     }
-    pop(){
+    pop(): object{
         const len: number = this.length;
         let removeData;
         if(len){
@@ -19,18 +19,33 @@ class Stack<T>{
             return removeData;
         }
     }
-    toString(){
+    toString(): string{
 
         let str: string = '';
-        for(let i: number = 1; i < this.length; i++){
-            if(i + 1!== this.length){
-                str += this.storage[i] + ', ';
-            }
-            else{
-                str += this.storage[i];
-            }
+        for(let i: number = 1; i <= this.length; i++){
+            str += this.storage[i] + ' ';
         }
         return str;
     }
+    hashcode(): number{
+        const str = this.toString();
+        console.log(str);
+        let hash: number = 0;
+        for(let i = 0; i < str.length; i++){
+            let character = str.charCodeAt(i);
+            hash += character;
 
+        }
+        return hash;
+    }
+    equals(obj: any){
+        if(obj.hashcode() === this.hashcode()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
+
+
